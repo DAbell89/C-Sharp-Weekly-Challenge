72df8bd0930f9ca8c20e3d1f538b51071fc37bab
David Abell <d-abell@live.com>
2016-09-12 13:42:46 -0400

    Code Challenge Week 2

--- Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
+++ Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace CodeLou.CSharp.Week2.Challenge
 {
@@ -16,7 +16,7 @@ namespace CodeLou.CSharp.Week2.Challenge
             // Hint: In Visual Studio, you can build the project by selecting "Build Solution" from the
             //       build menu, or by pressing <Ctrl>+<Shift>+B.
             Console.WriteLine("Welcome to the Code Louisville C# week 2 code challenge!");
-            Console.WriteLine("Press <Enter> to begin...")
+            Console.WriteLine("Press <Enter> to begin...");
             Console.ReadLine();
 
             Console.WriteLine("This is the launch application for the first human mission to Mars.");
@@ -26,24 +26,46 @@ namespace CodeLou.CSharp.Week2.Challenge
             // Capture the number of seconds that the user would like to count down before liftoff.
             // Hint: You should use another method of the Console class and store the output into a
             //       variable to use later.
+            var countMax = Console.ReadLine();
+            int countConverted = 0;
+            if (int.TryParse(countMax, out countConverted))
+            {
 
+                // Task 4:
+                // Write a condition to test whether the number that they entered is less than or equal to zero.
+                // Call the IsLessThanOrEqualToZero() method below, passing the user's number as a parameter.
+                // If the result is true, write "Please enter a positive number." to the console.
+                // Hint: The input that you captured is currently a string type. You will have to "parse" it
+                //       as a different type in order to pass it to the IsLessThanOrEqualToZero function.
+                if (IsLessThanOrEqualToZero(countConverted))
+                {
+                    Console.WriteLine("Please enter a positive number.");
+                }
+                else
+                {
 
-            // Task 4:
-            // Write a condition to test whether the number that they entered is less than or equal to zero.
-            // Call the IsLessThanOrEqualToZero() method below, passing the user's number as a parameter.
-            // If the result is true, write "Please enter a positive number." to the console.
-            // Hint: The input that you captured is currently a string type. You will have to "parse" it
-            //       as a different type in order to pass it to the IsLessThanOrEqualToZero function.
+                    // Task 5:
+                    // Add an "else" block to the condition from Task 4. This should be run in the case that the
+                    // number is greater than zero. Write each number to the console, counting backwards, from the 
+                    // user's number to zero. Then write, "LIFTOFF!".
+                    // Hint: You can accomplish this with one of several kinds of loops, including "while" and
+                    //       "for". You can choose whichever you'd like to solve the task. The Microsoft
+                    //       Developer Network (MSDN) website contains all of the documentation for C#. If you want
+                    //       to learn more about loops, visit https://msdn.microsoft.com/en-us/library/32dbftby.aspx.
+                    while (countConverted >= 0)
+                    {
+                        Console.WriteLine(countConverted);
+                        countConverted--;
+                    }
 
+                    Console.WriteLine("LIFTOFF!");
+                }
 
-            // Task 5:
-            // Add an "else" block to the condition from Task 4. This should be run in the case that the
-            // number is greater than zero. Write each number to the console, counting backwards, from the 
-            // user's number to zero. Then write, "LIFTOFF!".
-            // Hint: You can accomplish this with one of several kinds of loops, including "while" and
-            //       "for". You can choose whichever you'd like to solve the task. The Microsoft
-            //       Developer Network (MSDN) website contains all of the documentation for C#. If you want
-            //       to learn more about loops, visit https://msdn.microsoft.com/en-us/library/32dbftby.aspx.
+            }
+            else
+            {
+                Console.WriteLine("\"" + countMax + "\" is not a valid input.");
+            }
 
             Console.WriteLine("Press <Enter> to exit...");
             Console.ReadLine();
